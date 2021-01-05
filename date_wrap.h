fd9cbaa7c0603e802e6ed77e3a6f1fbc723bdcd1
gil-kapel <gil-kapel@campus.technion.ac.il>
2021-01-05 19:17:22 +0200

    test

---
+++ date_wrap.h
@@ -0,0 +1,29 @@
+#ifndef DATE_WRAP_H
+#define DATE_WRAP_H
+
+#include <>
+#include "date.h"
+
+test   
+class dateWrap{
+    int day;
+    int week;
+    int year;
+public:
+    dateWrap(Date date);
+//*    ~dateWrap();
+    int getDay();
+    int getMonth();
+    int getYear();
+    void printDate();
+    bool operator==(const dateWrap& date)const;
+    bool operator>=(const dateWrap& date);
+    bool operator<=(const dateWrap& date);
+    bool operator!=(const dateWrap& date);
+    bool operator>(const dateWrap& date);
+    bool operator<(const dateWrap& date);
+    dateWrap& operator++();
+    dateWrap& operator+=(int days);
+}
+
+dateWrap operator+(int days);
