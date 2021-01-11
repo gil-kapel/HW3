a307d056dccc7fa91dadd3935ad13aad126b986c
רותם <isrotem@campus.technion.ac.il>
2021-01-11 20:41:28 +0200

    this my first commit
    s

--- event_container.h
+++ event_container.h
@@ -1,1 +1,15 @@
 
+#ifndef BASE_EVENT_H_
+#define BASE_EVENT_H_
+
+#include "date_wrap.h"
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+using std::string;
+
+h
+// EventContaine
+// add
+// begin
+// end
