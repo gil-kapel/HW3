ae52637ab7f71a89aca75bfeb569ba3a5f937fad
gil-kapel <gil-kapel@campus.technion.ac.il>
2021-01-14 00:42:31 +0200

    generic linked list

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include "linked_list.h"
 #include <iostream>
+#include <stdio.h>
 using mtm::LinkedList;
-using std::cout;
-using std::cerr;
-using std::cin;
-using std::endl;
+using namespace std;
 int main()
 {
-	LinkedList members_list;
+	LinkedList<int> members_list;
 	members_list.insert(-12);
 	members_list.insert(1);
 	members_list.insert(4);
@@ -15,7 +13,13 @@ int main()
 	members_list.insert(12552);
 	members_list.insert(2333);
 	members_list.removeData(4);
-	LinkedList new_list;
+	if(members_list.contains(2))
+	{
+		printf("failure %d",members_list.get(5));
+	}
+	else printf("success %d",members_list.get(5));
+
+	LinkedList<int> new_list;
 	new_list = members_list;
 	cout << new_list << endl;
 	return 0;
