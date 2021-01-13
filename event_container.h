4a4d4d63c14b2d223b6bfb800e486b83a091bdad
רותם <isrotem@campus.technion.ac.il>
2021-01-13 16:57:37 +0200

    comment

--- event_container.h
+++ event_container.h
@@ -3,22 +3,67 @@
 
 #include "date_wrap.h"
 #include "priority_queue.h"
+#include "base_event.h"
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
 using std::string;
 using mtm::PriorityQueue;
 
-// namespace mtm{
+namespace mtm{
     class EventContainer{
     // protected:
-        PriorityQueue<int> membersList(); 
+    //     PriorityQueue<int> membersList(); 
     public:
-        EventContainer();// = default
-        EventContainer& operator = (const EventContainer&)
-        virtual ~EventContainer();
+        EventContainer(){//(DateWrap date, string name){
+            // this->date = date;
+            // this->name = name;
+            // this->membersList = mtm::PriorityQueue<int>::PriorityQueue();
+        }
+        virtual EventContainer(const EventContainer&) {} // copy constructor
+        virtual ~EventContainer() {} // d'tor
+        EventContainer& EventContainer::operator= (const EventContainer& ec){ // assignment operator
+            if(this != &ec){
+                throw self_assign;
+            }
+            return *this;
+        //BaseEvent& operator=(const BaseEvent& event)
+        //{
+            // if(this == &event)
+            // {
+            // return *this;
+            // // }
+            // dateDestroy(date);
+            // this->date = event.date;
+            // this->name = event.name;
+            // this->membersList = event.membersList;
+            // return *this;
+        //}
+        virtual void add(BaseEvent&)
+        {
+            mtm::PriorityQueue<int>::add(BaseEvent);//??
+        }
+        virtual void begin()
+        {
+            //mtm::PriorityQueue<int>::()
+        }
+        virtual void end()
+        {
+           return EventContainer::EventIterator()
+        }
+        // virtual std::ostream& printLong(std::ostream& os)
+        // {
+        //     return os << printShort() << endl << this->membersList << endl;
+        // }
+        // virtual BaseEvent* clone() const = 0{
+        //     BaseEvent new_event(date, name, membersList);
+        //     new_event.membersList = membersList;
+        //     return *new_event;
+        // }
+        // PriorityQueue<int> getMembersList()
+        // {
+        //     return membersList;
+        // }
     };
-
-} //* End of mtm namespace*/
-
+}
 #endif //EVENT_CONTAINER_H_
