be5c8586ed35ecfabf7cf0af70042c9d930bd068
רותם <isrotem@campus.technion.ac.il>
2021-01-13 19:04:09 +0200

    change the iterator, check where it should be and use getfirst in begin

--- event_container.h
+++ event_container.h
@@ -16,30 +16,21 @@ namespace mtm{
     //     PriorityQueue<int> membersList(); 
     public:
         EventContainer(){//(DateWrap date, string name){
-            // this->date = date;
-            // this->name = name;
             // this->membersList = mtm::PriorityQueue<int>::PriorityQueue();
         }
         virtual EventContainer(const EventContainer&) {} // copy constructor
         virtual ~EventContainer() {} // d'tor
-        EventContainer& EventContainer::operator= (const EventContainer& ec){ // assignment operator
+        EventContainer:EventIterator(){
+            EventIterator& EventUterator::operator= (const EventIterator& it){ // assignment operator
             if(this == &ec){
                 return *this;            
             }
-        EventContainer& operator++ (); 
-        EventIterator::EventIterator()
-        //BaseEvent& operator=(const BaseEvent& event)
-        //{
-            // if(this == &event)
-            // {
-            // return *this;
-            // // }
-            // dateDestroy(date);
-            // this->date = event.date;
-            // this->name = event.name;
-            // this->membersList = event.membersList;
-            // return *this;
-        //}
+            EventIterator& operator++(){
+
+            }
+
+
+        }
         virtual void add(BaseEvent&)
         {
             mtm::PriorityQueue<int>::add(BaseEvent);//??
@@ -52,10 +43,6 @@ namespace mtm{
         {
            return EventContainer::EventIterator()
         }
-        // virtual std::ostream& printLong(std::ostream& os)
-        // {
-        //     return os << printShort() << endl << this->membersList << endl;
-        // }
         // virtual BaseEvent* clone() const = 0{
         //     BaseEvent new_event(date, name, membersList);
         //     new_event.membersList = membersList;
