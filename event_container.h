1a9dea8e3bdffb1f9d7985015674a8798e36ae5b
רותם <isrotem@campus.technion.ac.il>
2021-01-16 19:38:15 +0200

    not final :x

--- event_container.h
+++ event_container.h
@@ -12,45 +12,115 @@ using mtm::PriorityQueue;
 
 namespace mtm{
     class EventContainer{
-    // protected:
-    //     PriorityQueue<int> membersList(); 
+    protected:
+        PriorityQueue <int> membersList; 
     public:
-        EventContainer(){//(DateWrap date, string name){
-            // this->membersList = mtm::PriorityQueue<int>::PriorityQueue();
-        }
+        EventContainer(PriorityQueue <int> membersList)
+        : membersList() // we want empty queue so there is nothing inside 
+        {}
         virtual EventContainer(const EventContainer&) {} // copy constructor
         virtual ~EventContainer() {} // d'tor
-        EventContainer:EventIterator(){
-            EventIterator& EventUterator::operator= (const EventIterator& it){ // assignment operator
+        virtual void add(BaseEvent&) = 0 {
+            mtm::PriorityQueue<int>::add(BaseEvent);//??
+        }
+        virtual void begin() = 0 {
+            return EventContainer::EventIterator()
+        }
+        virtual void end() = 0 {
+           return EventContainer::EventIterator()
+        }
+        class EventIterator{
+            public:
+            EventIterator& operator= (const EventIterator& it){ // assignment operator
             if(this == &ec){
                 return *this;            
             }
             EventIterator& operator++(){
-
             }
-
-
-        }
-        virtual void add(BaseEvent&)
-        {
-            mtm::PriorityQueue<int>::add(BaseEvent);//??
-        }
-        virtual void begin()
-        {
-            return EventContainer::EventIterator()
-        }
-        virtual void end()
-        {
-           return EventContainer::EventIterator()
-        }
-        // virtual BaseEvent* clone() const = 0{
-        //     BaseEvent new_event(date, name, membersList);
-        //     new_event.membersList = membersList;
-        //     return *new_event;
-        // }
+            Number& Number::operator++ (){ // ++ operator
+            ++it; // is it right?
+            return *this;
+            }
+            EventIterator operator*(const EventContainer& ec)
+            {
+                return BaseEvent&; // reference to BaseEvent
+            }
+            bool operator==(const EventContainer& ec) const;
+            bool operator!=(const EventContainer& ec) const;
+            
+            
     };
-    Number& Number::operator++ (){
-        ++iter; // is it right?
-        return *this;
-    }
+
+  
+
+
+
+
+//     class Example {
+//     protected:
+//         int* array;
+//         int size;
+//     public:
+//         Example(int _size, int defaultValue)
+//         : array(initArray(size, defaultValue)), size(_size) 
+//         {}
+
+//         Example(const Example& other)
+//         : array(other.array), size(other.size)
+//         {}
+
+//         Example& operator=(const Example& other)
+//         {
+//             this->array = other.array;
+//             this->size = other.size;
+//         }
+
+//         void f(Example other){
+            
+//         }
+
+//         Example& generate(){
+//             return *this;
+//         }
+
+//         static int* initArray(int size, int defaultValue){
+
+//             array = new int[size];
+//             for(int i = 0 ; i < size; i++){
+//                 array[i] = defaultValue;
+//             }
+//             return array;
+//         }
+
+//     }
+
+// void g(){
+//     Example a;
+//     Example b(a);
+//     Example c = a;
+//     Example d = a.generate();
+
+
+// }
+// class ExtendedExample;
+
+// class ExtendedExample : public Example {
+//     int field;
+// public:
+//     ExtendedExample(int size)
+//     : array(initArray(size)), size(size), field(0)
+//     {}
+    
+//     ExtendedExample(int size)
+//     : Example(size, 5), field(0)
+//     {}
+
+//     void updateSomething() override {
+//         field++;
+//         Example::updateSomething();
+//     }
+
+// }
+
+
 #endif //EVENT_CONTAINER_H_
