e24d8a39e59838809385fd372d9f02c219b74041
רותם <isrotem@campus.technion.ac.il>
2021-01-17 17:45:16 +0200

    check which methods are still pure virtual

--- festival.h
+++ festival.h
@@ -7,7 +7,27 @@
 #include <string.h>
 #include <iostream>
 
-mtm:festival festival(mtm::datewrap)
+using std::string;
+using mtm::PriorityQueue;
+
+namespace mtm{
+    class Festival: public EventContainer{
+    public:
+    Festival(DateWrap <int>date) :
+        Festival::festival(DateWrap <int>date)  {} // we want empty queue so there is nothing inside 
+    void Festival::add(const BaseEvent& added_event){ // add new event if not similar
+        if(added_event.date != Festival.date){ // choose condition for exception
+            throw DataMismatch;// write exception
+        }
+        PriorityQueue::insert(added_event);
+    }
+    void Festival::begin(const BaseEvent& first_event){
+        return PriorityQueue::getFirst();
+    }
+    void Festival::end(const BaseEvent& added_event){
+       return PriorityQueue::getNext(); // is it the one *after* the last event??
+    }
+// mtm:festival festival(mtm::datewrap)
 
 
 #endif //FESTIVAL_H_
