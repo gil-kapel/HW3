14b01c8d420f1fa6934ef51da47b7a76354a3afc
RI-OP <73331173+RI-OP@users.noreply.github.com>
2021-01-19 00:01:32 +0200

    Update festival.h

--- partB/festival.h
+++ partB/festival.h
@@ -25,142 +25,14 @@ namespace mtm{
     ~Festival(){}
     Festival& operator=(const Festival& festival);
     void add(const BaseEvent& new_event) override;// add new event if not similar
-    void Festival::begin();
-    void Festival::end();
+    void begin();
+    void end();
     }
 };
 
 
-// // mtm:festival festival(mtm::datewrap)
 
 
-#endif //FESTIVAL_H_
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// #ifndef FESTIVAL_H_
-// #define FESTIVAL_H_
-
-// #include "date_wrap.h"
-// #include "event_container.h"
-// #include "event_base.h"
-
-// #include <stdlib.h>
-// #include <string.h>
-// #include <iostream>
-
-
-// namespace mtm{
-//     class Festival: public EventContainer{
-//     public:
-//     Festival(DateWrap <int>date) :
 //         Festival::festival(DateWrap <int>date)  {} // we want empty queue so there is nothing inside 
 //     void Festival::add(const BaseEvent& added_event) override{ // add new event if not similar
 //         if(added_event.date != Festival.date){ // choose condition for exception
@@ -174,7 +46,9 @@ namespace mtm{
 //     void Festival::end(const BaseEvent& last_event){
 //        return LinkedList::getNext(); // is it the one *after* the last event??
 //     }
+
+
 // // mtm:festival festival(mtm::datewrap)
 
 
-// #endif //FESTIVAL_H_
+#endif //FESTIVAL_H_
