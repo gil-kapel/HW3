c078f051594cf7faa4d64539399350e9a74a2c95
RI-OP <73331173+RI-OP@users.noreply.github.com>
2021-01-18 21:55:01 +0200

    Update festival.h

--- partB/festival.h
+++ partB/festival.h
@@ -1,21 +1,33 @@
 #ifndef FESTIVAL_H_
 #define FESTIVAL_H_
 
+#include "date_wrap.h"
 #include "event_container.h"
+#include "event_base.h"
 
-using std::string;
-using mtm::LinkedList;
-using mtm::EventContainer;
-using mtm::DateWrap;
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
 
 
 namespace mtm{
-    class Festival : public EventContainer{
-        Date festival_date;
+    class Festival: public EventContainer{
     public:
-        Festival(Date date);
-        Festival(const Festival& festival);
-        void add(const BaseEvent& new_event) override;
-    };
+    Festival(DateWrap <int>date) :
+        Festival::festival(DateWrap <int>date)  {} // we want empty queue so there is nothing inside 
+    void Festival::add(const BaseEvent& added_event) override{ // add new event if not similar
+        if(added_event.date != Festival.date){ // choose condition for exception
+            throw DataMismatch;// write exception
+        }
+        LinkedList::insert(added_event);
+    }
+    void Festival::begin(const BaseEvent& first_event){
+        return LinkedList::getFirst();
+    }
+    void Festival::end(const BaseEvent& last_event){
+       return LinkedList::getNext(); // is it the one *after* the last event??
+    }
+// mtm:festival festival(mtm::datewrap)
+
 
 #endif //FESTIVAL_H_
