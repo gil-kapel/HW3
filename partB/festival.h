e965c80cc245ce1eaca2e0a27710c5d0d3b1042f
gil-kapel <74864454+gil-kapel@users.noreply.github.com>
2021-01-18 12:25:34 +0200

    Update festival.h

--- partB/festival.h
+++ partB/festival.h
@@ -1,33 +1,21 @@
 #ifndef FESTIVAL_H_
 #define FESTIVAL_H_
 
-#include "date_wrap.h"
-#include "priority_queue.h"
-#include <stdlib.h>
-#include <string.h>
-#include <iostream>
+#include "event_container.h"
 
 using std::string;
-using mtm::PriorityQueue;
+using mtm::LinkedList;
+using mtm::EventContainer;
+using mtm::DateWrap;
+
 
 namespace mtm{
-    class Festival: public EventContainer{
+    class Festival : public EventContainer{
+        Date festival_date;
     public:
-    Festival(DateWrap <int>date) :
-        Festival::festival(DateWrap <int>date)  {} // we want empty queue so there is nothing inside 
-    void Festival::add(const BaseEvent& added_event) override{ // add new event if not similar
-        if(added_event.date != Festival.date){ // choose condition for exception
-            throw DataMismatch;// write exception
-        }
-        PriorityQueue::insert(added_event);
-    }
-    void Festival::begin(const BaseEvent& first_event){
-        return PriorityQueue::getFirst();
-    }
-    void Festival::end(const BaseEvent& last_event){
-       return PriorityQueue::getNext(); // is it the one *after* the last event??
-    }
-// mtm:festival festival(mtm::datewrap)
-
+        Festival(Date date);
+        Festival(const Festival& festival);
+        void add(const BaseEvent& new_event) override;
+    };
 
 #endif //FESTIVAL_H_
