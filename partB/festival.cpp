b065c403907d669186f7e42d741497648ab9d763
RI-OP <73331173+RI-OP@users.noreply.github.com>
2021-01-19 17:34:02 +0200

    Update festival.cpp

--- partB/festival.cpp
+++ partB/festival.cpp
@@ -19,34 +19,57 @@ using mtm::LinkedList;
 using mtm::BaseEvent;
 using mtm::Festival;
 
-Festival(DateWrap date):
+Festival::Festival(DateWrap date):
 festival.date = date{
-
 }
+
+Festival::Festival(const Festival& festival): 
+    date(DateWrap(festival.date)) {
+}
+
+Festival& Festival::operator=(const Festival& festival)
+{
+    if (this == &festival)
+    {
+        return this*;
+    }
+    events_list = festival.events_list;
+    date = festival.date;
+    return this*;
+}
+
+Festival::~Festival(){}
 
 void Festival::add(const BaseEvent& event)
 {
     if(events_list.contains(event))
+    {
+        throw mtm::Notsupported();
+    }
+    if(festival.date != event.date)
     {
         throw mtm::DateMismatch();
     }
     events_list.insert(event);
 } 
 
+// begin and end
 
-        Festival::festival(DateWrap <int>date)  {} // we want empty queue so there is nothing inside 
-    void Festival::add(const BaseEvent& added_event) override{ // add new event if not similar
-        if(added_event.date != Festival.date){ // choose condition for exception
-            throw DataMismatch;// write exception
-        }
-        LinkedList::insert(added_event);
-    }
-    void Festival::begin(const BaseEvent& first_event){
-        return LinkedList::getFirst();
-    }
-    void Festival::end(const BaseEvent& last_event){
-       return LinkedList::getNext(); // is it the one *after* the last event??
-    }
+
+
+    //     Festival::festival(DateWrap <int>date)  {} // we want empty queue so there is nothing inside 
+    // void Festival::add(const BaseEvent& added_event) override{ // add new event if not similar
+    //     if(added_event.date != Festival.date){ // choose condition for exception
+    //         throw DataMismatch;// write exception
+    //     }
+    //     LinkedList::insert(added_event);
+    // }
+    // void Festival::begin(const BaseEvent& first_event){
+    //     return LinkedList::getFirst();
+    // }
+    // void Festival::end(const BaseEvent& last_event){
+    //    return LinkedList::getNext(); // is it the one *after* the last event??
+    // }
 
 
 
