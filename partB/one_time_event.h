56cc6e069a12b0b8bb84b26eb93bc7e2c9efdf89
RI-OP <73331173+RI-OP@users.noreply.github.com>
2021-01-20 13:50:28 +0200

    Update one_time_event.h

--- partB/one_time_event.h
+++ partB/one_time_event.h
@@ -2,16 +2,46 @@
 #define ONE_TIME_EVENT_
 
 #include "event_container.h"
+#include "base_event.h"
+
+using std::string;
+using mtm::LinkedList;
+using mtm::EventContainer;
+using mtm::BaseEvent;
+using mtm::DateWrap;
+using mtm::OneTimeEvent;
+
 
 namespace mtm{
     template<class EventType>
     class OneTimeEvent : public EventContainer{
         EventType event;
+        string name;
+        Date date;
     public:
-        OneTimeEvent(Date date, string name);
-        OneTimeEvent(const OneTimeEvent& one_time_event);
-        void add(const BaseEvent& new_event) override;
+        OneTimeEvent() = default;// we have another C'tor in the class, despite this we need this "default" C'tor 
+        OneTimeEvent(EventType event, Date date, string name);
+        OneTimeEvent(const OneTimeEvent& one_time_event) = default;
+        RecurringEvent& operator=(const RecurringEvent&) = default;
+        ~OneTimeEvent() = default;
+        void add(const BaseEvent* new_event) override;
     };
 
-#endif //ONE_TIME_EVENT_
+    template<class EventType>
+    OneTimeEvent<EventType>::OneTimeEvent(EventType event, Date date, string name):
+        event(event), name(name), date(date){
+            event = EventType(event)
+        }
+    
+    template<claa EventType>
+    void OneTimeEvent<EventType>::add(BaseEvent* new_event){
+        if(event_list.contains(new_event)){
+            throw NotSupported;
+        }
+        events_list.insert(new_event)
+    }
+    
 
+};
+
+#endif //ONE_TIME_EVENT_
