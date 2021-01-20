2a35aaf4c441c0b750a7c28e7f11f1115b57ee4c
RI-OP <73331173+RI-OP@users.noreply.github.com>
2021-01-20 13:52:11 +0200

    Update recurring_event.h

--- partB/recurring_event.h
+++ partB/recurring_event.h
@@ -2,6 +2,14 @@
 #define RECURRING_EVENT_H
 
 #include "event_container.h"
+#include "base_event.h"
+
+using std::string;
+using mtm::LinkedList;
+using mtm::EventContainer;
+using mtm::BaseEvent;
+using mtm::DateWrap;
+using mtm::RecurringEvent;
 
 namespace mtm{
     template <class EventType>
@@ -10,7 +18,7 @@ namespace mtm{
         string name;
         int num_occurrences;
         int interval_days;
-        DateWrap first_date;
+        Date first_date;
     public:
         RecurringEvent() = default;// we have another C'tor in the class, despite this we need this "default" C'tor 
         RecurringEvent(EventType event, string name, int num_occurrences, int interval_days, Date first_date);
@@ -23,22 +31,25 @@ namespace mtm{
     template<class EventType>
     RecurringEvent<EventType>::RecurringEvent(EventType event, string name, int num_occurrences, int interval_days, Date first_date): 
         event(event), name(name), num_occurrences(num_occurrences), interval_days(interval_days), first_date(first_date) {
-        if (num_occurrences < 1){
-            throw mtm::InvalidNumber();
-        }
-        if (interval_days < 1){
-            throw mtm::InvalidDate();
-        }
+    if (num_occurrences < 1){
+        throw mtm::InvalidNumber();
+    }
+    if (interval_days < 1){
+        throw mtm::InvalidDate();
+    }
+    event = EventType(event)//
     }
 
     template<class EventType>
     void RecurringEvent<EventType>::add(BaseEvent* new_event){
-        if(events_list.contains(event))
+        if(events_list.contains(new_event))
         {
             throw mtm::NotSupported();
         }
-        events_list.insert(new_event->clone());
+        events_list.insert(new_event);
     }
+
 }
 
 #endif //RECURRING_EVENT_H
+
